f2e95dc895c9ba5d76f7ff5d3fba5a44287c9bf2
nawarelhaouat <nawarelhaouat@gmail.com>
2026-02-07 00:25:28 +0100

    implement react confetti

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {useState} from "react"
 import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
 import Die from './components/Die'
 
 
@@ -42,6 +43,7 @@ const App = () => {
 
   return (
     <main>
+      {gameWon && <Confetti />}
       <div className="main--container">
         <h1>Tenzies</h1>
         <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
